3050813108ab3f45efff25aa2891e2326c9dcf3d
rahimz <rahim.aghareb@gmail.com>
2019-05-16 15:20:06 +0430

    model of product and Image updated

--- products/models.py
+++ products/models.py
@@ -4,12 +4,21 @@ from django.db import models
 
 
 class Product(models.Model):
+	STATUS_CHOICES = (
+    ('men', 'Men'),
+    ('women', 'Women'),
+    )
 	productName = models.CharField(max_length=250)
-	productGenre = models.CharField(max_length=250)
+	productGenre = models.CharField(max_length=50,
+									choices=STATUS_CHOICES, 
+									default='draft')
 	productGroup = models.CharField(max_length=250)
 	productSize = models.CharField(max_length=250)
-	productDescript = models.CharField(max_length=250)
+	productDescript = models.TextField()
 	productImage = models.FileField(null=True, blank=True)
+	imageAlt = models.CharField(max_length=250,
+								null=True,
+								blank=True)
 	productPublish = models.DateTimeField('date published')
 
 
@@ -18,13 +27,15 @@ class Product(models.Model):
 
 
 class Image(models.Model):
-	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	product = models.ForeignKey(Product,
+								related_name='image',
+								on_delete=models.CASCADE)
 	#productName = self.productName
-	imageMain = models.FileField(null=True, blank=True)
+	image = models.ImageField(null=True, blank=True)
 	votes = models.IntegerField(default=0)
 
 	def __str__(self):
-		return str(self.imageMain) + ' - ' + str(self.product_id)
+		return str(self.product_id)
 
 '''
 class Question(models.Model):
