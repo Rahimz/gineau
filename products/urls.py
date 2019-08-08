6accfc7e60b69a6fc09f88faa54850e864eed414
rahimz <rahim.aghareb@gmail.com>
2019-08-08 18:48:43 +0430

    bug in url fixed

--- products/urls.py
+++ products/urls.py
@@ -8,9 +8,9 @@ app_name = 'products'
 urlpatterns = [
     # path('', views.IndexView.as_view(), name='index'),
     path('', views.HomeView, name='home'),
+    path('products/<int:pk>/', views.DetailView, name='detail'),
     path('products/<str:genre>/', views.ProductListView, name='productlistview'),
     path('tag/<slug:tag_slug>/', views.ProductListView, name='product_list_by_tag'),
-    path('<int:pk>/', views.DetailView, name='detail'),
     path('user/email/', views.add_new_email, name='email_add'),
   
     path('<int:product_id>/images/', views.prdimage, name='prdimage'),
